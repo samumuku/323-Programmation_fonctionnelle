d9fe51088a74e05dba68fcf55fcd536d226082f6
samumuku <pr13xpj@eduvaud.ch>
2025-09-24 10:57:35 +0200

    feat(rando): Transformer la liste pour obtenir une liste de points graphiques (`System.Drawing.Point`)

--- personnel/exo/rando/RandoBase/Rando/Rando/Rando.cs
+++ personnel/exo/rando/RandoBase/Rando/Rando/Rando.cs
@@ -17,7 +17,7 @@ namespace Rando
                 .Descendants(ns + "trkpt") //descendant va retourner une collection filtré avec la balise "trkpt" qui se trouve dans les fichiers gpx
                 .Select(x => new Trackpoint(
                     (double)x.Attribute("lat"), // le (double) est une conversion explicite que nous pouvons faire avec LinQ à XML (xattribute, xelement)
-                    (double)x.Attribute("lon"), // xattribute va chercher les attributs qui sont dans le <trkpt>, donc "lon" et "lat"
+                    (double)x.Attribute("lon"),// xattribute va chercher les attributs qui sont dans le <trkpt>, donc "lon" et "lat"
                     (double)x.Element(ns + "ele") // xelement va chercher l'élément enfant qui se trouve aussi dans <trkpt> mais, celui là est le "ele"
                 )).ToList();
         } //regarder la doc sur https://weblogs.asp.net/jimjackson/using-linq-to-xml-with-c-to-read-gpx-files/
@@ -25,22 +25,24 @@ namespace Rando
         public Rando()
         {
             InitializeComponent();
-            // emplacement du fichier gpx
-            trackpoints = ReadGpx("../../../../../../gpx/gemmikandersteg.gpx");
-
-            foreach(Trackpoint trackpoint in trackpoints)
-            {
-                Debug.WriteLine(trackpoint);
-            }
         }
 
         private void Rando_Form_Paint(object sender, PaintEventArgs e)
         {
-            Pen myPen = new Pen(Color.Red);
-            myPen.Width = 2;
+            // emplacement du fichier gpx
+            trackpoints = ReadGpx("../../../../../../gpx/gemmikandersteg.gpx");
 
-            Point[] points = new Point[4] { new Point(30,50), new Point(50,10), new Point(80,50), new Point(111,400) };
-            this.CreateGraphics().DrawLines(myPen, points);
+            List<Point> filteredTrackpoints = trackpoints.Select(trackpoint => new Point((int)((trackpoint.Latitude-46.39)*6500), (int)((trackpoint.Longitude-7.61)*6500))).ToList();
+
+            foreach (Point point in filteredTrackpoints)
+            {
+                Debug.WriteLine(point.ToString());
+            }
+            // dessiner
+            /*Pen myPen = new Pen(Color.Red);
+            myPen.Width = 2;
+            Point[] filteredPoints = filteredTrackpoints.ToArray();
+            this.CreateGraphics().DrawLines(myPen, filteredPoints);*/
         }
     }
 }
